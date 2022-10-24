dc3b6ea05e9186408d11fcedef6984ef3f8825c9
caicodes <caicodes1@gmail.com>
2022-10-24 17:11:18 -0600

    added @headlessui/react@latest and implemented ListBox

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,9 +1,21 @@
 import { useState, useEffect } from 'react'
+import { Listbox } from '@headlessui/react'
 import { useTheme } from 'next-themes'
 
+const people = [
+  { id: 1, name: 'Durward Reynolds', unavailable: false },
+  { id: 2, name: 'Kenton Towne', unavailable: false },
+  { id: 3, name: 'Therese Wunsch', unavailable: false },
+  { id: 4, name: 'Benedict Kessler', unavailable: true },
+  { id: 5, name: 'Katelyn Rohan', unavailable: false },
+]
+
 const ThemeSwitch = () => {
+
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  const [selectedPerson, setSelectedPerson] = useState(people[0])
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -15,12 +27,35 @@ const ThemeSwitch = () => {
   }
 
   return (
-    <select value={theme} onChange={e => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
-    </select>
+    <>
+
+      <div className='flex flex-col w-1/2 mt-5 mx-auto gap-3'>
+        <select value={theme} onChange={e => setTheme(e.target.value)}>
+          <option value="system">System</option>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+        </select>
+
+        <Listbox value={selectedPerson} onChange={setSelectedPerson}>
+          <Listbox.Button>{selectedPerson.name}</Listbox.Button>
+          <Listbox.Options>
+            {people.map((person) => (
+              <Listbox.Option
+                key={person.id}
+                value={person}
+                disabled={person.unavailable}
+              >
+                {person.name}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
+      </div>
+
+
+    </>
   )
 }
+
 
 export default ThemeSwitch
