58d4374dce6dc9e698726c28c253779cf7d124e7
caicodes <caicodes1@gmail.com>
2022-10-24 18:15:34 -0600

    layout adjust for demo, listbox, radio, icons in

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,14 +1,15 @@
-import { useState, useEffect } from 'react'
-import { Listbox, RadioGroup } from '@headlessui/react'
+import { Fragment, useState, useEffect } from 'react'
+import { Listbox, RadioGroup, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { useTheme } from 'next-themes'
-import { CheckIcon } from '@heroicons/react/24/solid'
 
 const people = [
-  { id: 1, name: 'Durward Reynolds', unavailable: false },
-  { id: 2, name: 'Kenton Towne', unavailable: false },
-  { id: 3, name: 'Therese Wunsch', unavailable: false },
-  { id: 4, name: 'Benedict Kessler', unavailable: true },
-  { id: 5, name: 'Katelyn Rohan', unavailable: false },
+  { name: 'Wade Cooper' },
+  { name: 'Arlene Mccoy' },
+  { name: 'Devon Webb' },
+  { name: 'Tom Cook' },
+  { name: 'Tanya Fox' },
+  { name: 'Hellen Schmidt' },
 ]
 
 const plans = [
@@ -37,8 +38,10 @@ const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
+  const [selected, setSelected] = useState(people[0])
+
   const [selectedPerson, setSelectedPerson] = useState(people[0])
-  const [selected, setSelected] = useState(plans[0])
+  const [selectedPlan, setSelectedPlan] = useState(plans[0])
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -53,30 +56,61 @@ const ThemeSwitch = () => {
     <>
 
       <div className='flex flex-col w-1/2 mt-5 mx-auto gap-3'>
-        <select value={theme} onChange={e => setTheme(e.target.value)}>
-          <option value="system">System</option>
-          <option value="dark">Dark</option>
-          <option value="light">Light</option>
-        </select>
 
-        <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-          <Listbox.Button>{selectedPerson.name}</Listbox.Button>
-          <Listbox.Options>
-            {people.map((person) => (
-              <Listbox.Option
-                key={person.id}
-                value={person}
-                disabled={person.unavailable}
-              >
-                {person.name}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
+
+        <Listbox value={selected} onChange={setSelected}>
+          <div className="relative mt-1">
+            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+              <span className="block truncate">{selected.name}</span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {people.map((person, personIdx) => (
+                  <Listbox.Option
+                    key={personIdx}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                      }`
+                    }
+                    value={person}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span
+                          className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                            }`}
+                        >
+                          {person.name}
+                        </span>
+                        {selected ? (
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
         </Listbox>
 
 
 
-        <RadioGroup value={selected} onChange={setSelected}>
+
+        <RadioGroup value={selectedPlan} onChange={setSelectedPlan}>
           <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
           <div className="space-y-2">
             {plans.map((plan) => (
@@ -130,6 +164,15 @@ const ThemeSwitch = () => {
             ))}
           </div>
         </RadioGroup>
+        <div className='my-20'>
+          non-headless..
+        </div>
+        <select value={theme} onChange={e => setTheme(e.target.value)}>
+          <option value="system">System</option>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+        </select>
+
 
       </div>
 
