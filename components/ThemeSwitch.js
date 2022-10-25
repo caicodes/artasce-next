3961ffc363cf8792533b81138043ea64bfc55dcd
caicodes <caicodes1@gmail.com>
2022-10-24 18:00:42 -0600

    added radiogroup from headlessui for stying as theme toggle

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
-import { Listbox } from '@headlessui/react'
+import { Listbox, RadioGroup } from '@headlessui/react'
 import { useTheme } from 'next-themes'
+import { CheckIcon } from '@heroicons/react/24/solid'
 
 const people = [
   { id: 1, name: 'Durward Reynolds', unavailable: false },
@@ -10,12 +11,34 @@ const people = [
   { id: 5, name: 'Katelyn Rohan', unavailable: false },
 ]
 
+const plans = [
+  {
+    name: 'Startup',
+    ram: '12GB',
+    cpus: '6 CPUs',
+    disk: '160 GB SSD disk',
+  },
+  {
+    name: 'Business',
+    ram: '16GB',
+    cpus: '8 CPUs',
+    disk: '512 GB SSD disk',
+  },
+  {
+    name: 'Enterprise',
+    ram: '32GB',
+    cpus: '12 CPUs',
+    disk: '1024 GB SSD disk',
+  },
+]
+
 const ThemeSwitch = () => {
 
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
   const [selectedPerson, setSelectedPerson] = useState(people[0])
+  const [selected, setSelected] = useState(plans[0])
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -50,6 +73,64 @@ const ThemeSwitch = () => {
             ))}
           </Listbox.Options>
         </Listbox>
+
+
+
+        <RadioGroup value={selected} onChange={setSelected}>
+          <RadioGroup.Label className="sr-only">Server size</RadioGroup.Label>
+          <div className="space-y-2">
+            {plans.map((plan) => (
+              <RadioGroup.Option
+                key={plan.name}
+                value={plan}
+                className={({ active, checked }) =>
+                  `${active
+                    ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
+                    : ''
+                  }
+                  ${checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
+                  }
+                    relative flex cursor-pointer rounded-lg px-5 py-4 shadow-md focus:outline-none`
+                }
+              >
+                {({ active, checked }) => (
+                  <>
+                    <div className="flex w-full items-center justify-between">
+                      <div className="flex items-center">
+                        <div className="text-sm">
+                          <RadioGroup.Label
+                            as="p"
+                            className={`font-medium  ${checked ? 'text-white' : 'text-gray-900'
+                              }`}
+                          >
+                            {plan.name}
+                          </RadioGroup.Label>
+                          <RadioGroup.Description
+                            as="span"
+                            className={`inline ${checked ? 'text-sky-100' : 'text-gray-500'
+                              }`}
+                          >
+                            <span>
+                              {plan.ram}/{plan.cpus}
+                            </span>{' '}
+                            <span aria-hidden="true">&middot;</span>{' '}
+                            <span>{plan.disk}</span>
+                          </RadioGroup.Description>
+                        </div>
+                      </div>
+                      {checked && (
+                        <div className="shrink-0 text-white">
+                          <CheckIcon className="h-6 w-6" />
+                        </div>
+                      )}
+                    </div>
+                  </>
+                )}
+              </RadioGroup.Option>
+            ))}
+          </div>
+        </RadioGroup>
+
       </div>
 
     </>
